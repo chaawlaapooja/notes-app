e64f177cb7d76f35ceb3527a26c282bb70d0f071
chaawlaapooja <chaawlaapooja@gmail.com>
2022-10-07 07:42:55 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -5,6 +5,4 @@ import '../imports/api/users';
 import '../imports/api/notes';
 import '../imports/startup/simple-schema-configuration.js';
 
-Meteor.startup(() => {
-
-});
+Meteor.startup(() => {});
